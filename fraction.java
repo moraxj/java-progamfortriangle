2f93ee39f6cc4d8671c9cbf418e1446defe26bf6
moraxj <112534126+moraxj@users.noreply.github.com>
2022-08-31 11:12:25 +0530

    Add files via upload

---
+++ fraction.java
@@ -0,0 +1,10 @@
+public class fraction {
+    public void raction(int n,double s,int a){
+        for(int i = 1;i<=n;i++){
+            s =s+ (1/(Math.pow(a, i)));
+        
+         
+        }
+        System.out.println(s);
+    }
+}
