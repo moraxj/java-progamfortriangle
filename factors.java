444c96f83256c977fa07a03ed78a43a26a632c4c
moraxj <112534126+moraxj@users.noreply.github.com>
2022-09-02 10:33:15 +0530

    Add files via upload

---
+++ factors.java
@@ -0,0 +1,9 @@
+public class factors {
+    public void factp(int n){
+        for(int i = 1;i<=n;i++){
+            if(n%i==0)System.out.println(i);
+            else continue;
+
+        }
+    }
+}
